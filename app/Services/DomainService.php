057748beecedb9a86d512d69a37428012919c0f8
thangngo.dev <thangngo130802@gmail.com>
2024-09-05 09:10:16 +0700

    Merge branch 'main' of https://github.com/quangnvph26060/nhanhoa into Datgin
    # Conflicts:
    #	NhanHoa/routes/web.php
    #	app/Http/Controllers/Admin/CloudController.php
    #	app/Http/Controllers/Admin/DomainController.php
    #	app/Http/Controllers/Admin/HostingController.php
    #	app/Http/Controllers/Admin/PromotionController.php
    #	app/Http/Controllers/Admin/SslController.php
    #	app/Models/CloudPromotion.php
    #	app/Models/Domain.php
    #	app/Models/Domaintype.php
    #	app/Models/Hosting.php
    #	app/Models/Promotion.php
    #	app/Models/ServerPromotion.php
    #	app/Models/SgoHostingPromotion.php
    #	app/Models/Ssl.php
    #	app/Services/DomainService.php
    #	app/Services/HostingService.php
    #	app/Services/PromotionService.php
    #	app/Services/SslService.php
    #	database/migrations/2024_09_04_025434_create_sgo_hosting_promotion_table.php
    #	database/migrations/2024_09_04_065504_create_sgo_server_promotion_table.php
    #	database/migrations/2024_09_04_090626_create_sgo_cloud_promotion_table.php
    #	resources/views/admin/server/edit.blade.php

---
+++ app/Services/DomainService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Domain;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class DomainService
+{
+    protected $domain;
+
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+
+    public function getDomainAll(){
+        try {
+            Log::info('Fetching all domain');
+            return $this->domain->all();
+        } catch (Exception $e) {
+            Log::error('Failed to fetch domain: ' . $e->getMessage());
+            throw new Exception('Failed to fetch domain');
+        }
+    }
+
+    public function createDomain(array $data)
+    {
+        try {
+            DB::beginTransaction();
+            DB::commit();
+            $domain = $this->domain->create($data);
+            return $domain;
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error('Failed to create domain: ' . $e->getMessage());
+            throw new Exception('Failed to create domain');
+        }
+    }
+
+
+    public function updateDomain(array $data, $id): Domain
+    {
+        try {
+            DB::beginTransaction();
+            DB::commit();
+            $domain = $this->domain->find($id);
+            // dd($domain);
+            $domain->update($data);
+            return $domain;
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error('Failed to update domain: ' . $e->getMessage());
+            throw new Exception('Failed to update domain');
+        }
+    }
+    public function deleteDomain($id): Domain
+    {
+        try {
+            DB::beginTransaction();
+            DB::commit();
+            $domain = $this->domain->find($id);
+            $domain->delete();
+            return $domain;
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error('Failed to update domain: ' . $e->getMessage());
+            throw new Exception('Failed to update domain');
+        }
+    }
+}
