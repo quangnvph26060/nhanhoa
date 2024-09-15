057522a821069c49b0aa53d711e2c2a81077bae7
thangngo.dev <thangngo130802@gmail.com>
2024-09-15 20:36:20 +0700

    update

--- NhanHoa/app/Services/ServiceRegisterService.php
+++ NhanHoa/app/Services/ServiceRegisterService.php
@@ -11,6 +11,8 @@ use App\Models\GoogleWorkspaceBusinessPay;
 use App\Models\GoogleWorkspaceEducationPay;
 use App\Models\ServerLocation;
 use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class PromotionService
 {
@@ -30,6 +32,32 @@ class PromotionService
         $this->googleWorkspaceBusinessPay = $googleWorkspaceBusinessPay;
         $this->googleWorkspaceEducationPay = $googleWorkspaceEducationPay;
         $this->serverLocation = $serverLocation;
+    }
+
+    public function backup365RegisterAll(){
+
+        try {
+            DB::beginTransaction();
+            $backup365 = $this->backup365Pay->simplePaginate(10);
+            DB::commit();
+            return $backup365;
+        } catch (Exception $e) {
+            Log::error('Failed to all backup365 pay: ' . $e->getMessage());
+            throw new Exception('Failed to all backup365 pay');
+        }
+    }
+
+    public function cloudBackupRegisterAll(){
+
+        try {
+            DB::beginTransaction();
+            $cloudBackup = $this->cloudBackupPay->simplePaginate(10);
+            DB::commit();
+            return $cloudBackup;
+        } catch (Exception $e) {
+            Log::error('Failed to all cloudBackup pay: ' . $e->getMessage());
+            throw new Exception('Failed to all cloudBackup pay');
+        }
     }
 
 
